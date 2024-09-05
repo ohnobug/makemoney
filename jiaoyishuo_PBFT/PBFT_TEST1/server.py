6e427c008608c9e8a2274048992373c35999916c
Alex <2281551151@qq.com>
2024-09-05 16:21:16 +0800

    完成一点点

--- jiaoyishuo_PBFT/PBFT_TEST1/server.py
+++ jiaoyishuo_PBFT/PBFT_TEST1/server.py
@@ -1,8 +1,11 @@
 import argparse
 import asyncio
+from http import HTTPStatus
 import json
+import time
 import websockets
-from websockets.asyncio.server import serve
+from websockets.http11 import Request, Response
+from websockets.asyncio.server import serve, ServerConnection
 from websockets.asyncio.client import connect
 import yaml
 
@@ -22,40 +25,32 @@ def arg_parse():
 def conf_parse(conf_file) -> dict:
     return yaml.load(conf_file, Loader=yaml.SafeLoader)
 
-
-
 args = arg_parse()
 config = conf_parse(args.config)
 
-
-
-
-
+# 已经连接的节点数量
 nodes = {}
 
 class ServerNode:
-    def __init__(self, websocket):
+    def __init__(self, websocket: ServerConnection, id):
         self.websocket = websocket
-        print(websocket)
+        self.id = id
 
     async def handle(self):
         while True:
             message = await self.websocket.recv()
-            print("收到")
-            await self.websocket.send("回显: " + message)
+            client_ip, client_port =self.websocket.remote_address
 
+            print(f"我是{config['listen']['id']}, 我收到{self.id}的消息: \"{message}\"")
+            # await self.websocket.send("回显: " + message)
 
 # 服务端
 async def server(host, port):
-    async def handler(websocket):
+    async def handler(websocket: ServerConnection):
         try:
             # 收到对方的消息
             message = await websocket.recv()
-
             connect_info = json.loads(message)
-
-            print(f"服务器收到信息: {connect_info}")
-
             data = connect_info.get("data", None)
             if data is None:
                 return
@@ -83,7 +78,7 @@ async def server(host, port):
 
                 print("加入到节点列表中")
 
-                nodes[data["id"]] = ServerNode(websocket)
+                nodes[data["id"]] = ServerNode(websocket, data["id"])
 
                 print(f"现在有: {len(nodes)} 个节点与你相连")
 
@@ -100,12 +95,24 @@ async def server(host, port):
         except Exception as e:
             print(f"报错: {e}")
 
-    async with serve(handler, host, port):
+    def process_request(connection: ServerConnection, request: Request):
+        if request.path == '/health':
+            response = json.dumps({
+                "code": 200,
+                "message": f"当前连接节点: {len(nodes)}"
+            }, ensure_ascii=False)
+
+            return connection.respond(HTTPStatus.OK, text=response)
+
+    def process_response(connection, request, response):
+        response.headers["Content-Type"] = "application/json"
+
+    async with serve(handler, host, port, process_request=process_request, process_response=process_response):
         await asyncio.get_running_loop().create_future()
 
 # 客户端
 async def client(host, port):
-    async def client_handler(websocket):
+    async def client_handler(websocket: ServerConnection):
         # 收到对方的消息
         message = await websocket.recv()        
         print(f"客户端收到: {message}")
@@ -123,9 +130,8 @@ async def client(host, port):
             await websocket.close()
 
         if data is not None and (data.get("type") == "node") and (data.get("id") is not None and data.get("id") not in nodes):
-            nodes[data["id"]] = ServerNode(websocket)
+            nodes[data["id"]] = ServerNode(websocket, data["id"])
             await nodes[data["id"]].handle()
-
 
     try:
         async with connect(f"ws://{host}:{port}") as websocket:
@@ -154,15 +160,27 @@ async def client(host, port):
     except Exception as e:
         print(f"报错: {e}")
 
+
+async def sayHello():
+    while True:
+        await asyncio.sleep(3)
+        # print(len(nodes.values()))
+        # await nodes[0].websocket.send(f"hello: {time.time()}")
+        for node in nodes.values():
+            print(f"正在发送给{node.id}当前时间")
+            await node.websocket.send(f"当前时间: {time.time()}")
+            # await asyncio.sleep(1)
+
 async def main():
     serverTask = asyncio.create_task(server(config["listen"]["host"], config["listen"]["port"]))
 
     # 作为客户端, 连接其它节点
     tasks = [asyncio.create_task(client(nodeinfo["host"], nodeinfo["port"])) for nodeinfo in config["nodes"]]
+    sayTask = asyncio.create_task(sayHello())
 
     await serverTask
     await asyncio.gather(*tasks)
-
+    await sayTask
 
 asyncio.run(main())
 
