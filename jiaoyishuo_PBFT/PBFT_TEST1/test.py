c51836ddc0edcd06df9c3a217141582340da0b0b
Alex <2281551151@qq.com>
2024-09-09 15:36:47 +0800

    多节点连接成功

--- jiaoyishuo_PBFT/PBFT_TEST1/test.py
+++ jiaoyishuo_PBFT/PBFT_TEST1/test.py
@@ -1,23 +1,10 @@
 import asyncio
-import websockets
 
-async def websocket_handler(websocket, path):
-    print("来了")
-    try:
-        # 接收客户端消息
-        async for message in websocket:
-            print(f"Received message: {message}")
-            # 发送响应消息给客户端
-            await websocket.send(f"Server received: {message}")
-    except websockets.exceptions.ConnectionClosedError:
-        print("Client disconnected.")
+async def sayHello():
+    await asyncio.sleep(10)
+    print("hello")
 
-async def start_server(host, port):
-    async with websockets.serve(websocket_handler, host, port):
-        print(f"WebSocket server started on {host}:{port}")
-        await asyncio.get_running_loop().create_future()
+async def main():
+    asyncio.create_task(sayHello())
 
-if __name__ == "__main__":
-    host = "localhost"
-    port = 8765
-    asyncio.run(start_server(host, port))
+asyncio.run(main())
