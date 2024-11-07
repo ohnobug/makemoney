2f66ccd53a6fb4e83c392c5afb341b37a8cfbc3b
Alex <2281551151@qq.com>
2024-11-07 19:40:59 +0800

    ug

--- jiaoyishuoflutter3/lib/search.dart
+++ jiaoyishuoflutter3/lib/search.dart
@@ -405,82 +405,91 @@ class _LJNSearchPage extends State<LJNSearchPage> {
     return StoreConnector<StoreType, StoreType>(
         converter: (store) => store.state,
         builder: (context, vm) {
+          logger.info("pageControllerOffset $pageControllerOffset");
+
+          double mytop = 0;
+          if (pageControllerOffset > historyHeight) {
+            mytop = 0;
+          } else {
+            mytop = historyHeight - pageControllerOffset;
+          }
+
           return Scaffold(
-              primary: false,
-              appBar: PreferredSize(
-                  preferredSize: Size.fromHeight(90.0.w + _statusHeight),
-                  child: Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 0.w),
-                    margin: EdgeInsets.only(top: _statusHeight),
-                    height: 90.w,
-                    // color: const Color.fromARGB(255, 221, 76, 76), // 设置背景颜色
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () => Navigator.of(context).pop(), // 点击事件
-                          child: Container(
-                            // 加盒子是为了扩大点击区域
-                            color: Colors.transparent,
-                            child: Icon(
-                              const IconData(
-                                0xed9e,
-                                fontFamily: 'Iconfont',
-                              ), // 使用的图标
-                              color: Colors.black, // 图标颜色
-                              size: 36.w, // 图标大小
-                            ),
+            primary: false,
+            appBar: PreferredSize(
+                preferredSize: Size.fromHeight(90.0.w + _statusHeight),
+                child: Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.w, vertical: 0.w),
+                  margin: EdgeInsets.only(top: _statusHeight),
+                  height: 90.w,
+                  // color: const Color.fromARGB(255, 221, 76, 76), // 设置背景颜色
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.of(context).pop(), // 点击事件
+                        child: Container(
+                          // 加盒子是为了扩大点击区域
+                          color: Colors.transparent,
+                          child: Icon(
+                            const IconData(
+                              0xed9e,
+                              fontFamily: 'Iconfont',
+                            ), // 使用的图标
+                            color: Colors.black, // 图标颜色
+                            size: 36.w, // 图标大小
                           ),
                         ),
-                        Expanded(
-                            flex: 1,
-                            child: Container(
-                                // color: Colors.blue,
-                                margin: EdgeInsets.symmetric(horizontal: 15.w),
-                                height: 65.w,
-                                child: TextField(
-                                  onTapOutside: (event) {
-                                    FocusScope.of(context).unfocus();
-                                  },
-                                  cursorHeight: 35.w,
-                                  cursorWidth: 3.w,
-                                  decoration: InputDecoration(
-                                    prefixIcon: Icon(
-                                      const IconData(
-                                        0xe612,
-                                        fontFamily: 'Iconfont',
-                                      ),
-                                      color: Colors.black,
-                                      size: 40.w,
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                              // color: Colors.blue,
+                              margin: EdgeInsets.symmetric(horizontal: 15.w),
+                              height: 65.w,
+                              child: TextField(
+                                onTapOutside: (event) {
+                                  FocusScope.of(context).unfocus();
+                                },
+                                cursorHeight: 35.w,
+                                cursorWidth: 3.w,
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    const IconData(
+                                      0xe612,
+                                      fontFamily: 'Iconfont',
                                     ),
-                                    prefixIconConstraints: BoxConstraints(
-                                      minWidth: 70.w, // 控制图标与文字的最小宽度
-                                      // minHeight: 36.w,
-                                    ),
-                                    hintText: "搜索",
-                                    hintStyle: TextStyle(
-                                        fontWeight: FontWeight.normal,
-                                        fontSize: 30.w,
-                                        color: const Color.fromARGB(
-                                            255, 69, 75, 83)),
-                                    filled: true,
-                                    fillColor: const Color.fromARGB(
-                                        255, 217, 220, 224),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(30),
-                                      borderSide: BorderSide.none,
-                                    ),
-                                    contentPadding: EdgeInsets.symmetric(
-                                        vertical: 8.0.w, horizontal: 20.0.w),
+                                    color: Colors.black,
+                                    size: 40.w,
                                   ),
-                                ))),
-                      ],
-                    ),
-                  )),
-              body: Stack(children: [
-                // 列表
+                                  prefixIconConstraints: BoxConstraints(
+                                    minWidth: 70.w, // 控制图标与文字的最小宽度
+                                    // minHeight: 36.w,
+                                  ),
+                                  hintText: "搜索",
+                                  hintStyle: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 30.w,
+                                      color: const Color.fromARGB(
+                                          255, 69, 75, 83)),
+                                  filled: true,
+                                  fillColor:
+                                      const Color.fromARGB(255, 217, 220, 224),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 8.0.w, horizontal: 20.0.w),
+                                ),
+                              ))),
+                    ],
+                  ),
+                )),
+            body: Stack(
+              children: [
                 ScrollConfiguration(
                   behavior: ScrollConfiguration.of(context)
                       .copyWith(scrollbars: false),
@@ -729,7 +738,6 @@ class _LJNSearchPage extends State<LJNSearchPage> {
                           ],
                         ),
 
-                        // 占位
                         SizedBox(
                           height: 110.w,
                         ),
@@ -738,7 +746,6 @@ class _LJNSearchPage extends State<LJNSearchPage> {
                         SizedBox(
                           width: screenSize.width,
                           height: hotList.length * (72.w + 15.w) + 90.w,
-                          // height: 6000.w,
                           child: PageView(
                             scrollDirection: Axis.horizontal,
                             controller: hotListController,
@@ -762,12 +769,11 @@ class _LJNSearchPage extends State<LJNSearchPage> {
                     ),
                   ),
                 ),
-                // 标题
+
+                // 占位
                 Positioned(
                     left: 0,
-                    top: pageControllerOffset > historyHeight
-                        ? 0
-                        : (pageControllerOffset * -1) + historyHeight,
+                    top: mytop,
                     child: Container(
                       color: Colors.white,
                       key: hotTitleBoxKey,
@@ -813,8 +819,10 @@ class _LJNSearchPage extends State<LJNSearchPage> {
                           ],
                         ),
                       ),
-                    ))
-              ]));
+                    )),
+              ],
+            ),
+          );
         });
   }
 
