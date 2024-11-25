242aca2b28b2f88959be1d614f2e42e3b1ed3058
Alex <2281551151@qq.com>
2024-11-25 15:32:40 +0800

    升级

--- aiproject/lib/collectionAndPayment.dart
+++ aiproject/lib/collectionAndPayment.dart
@@ -94,7 +94,7 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                             Container(
                                 margin: EdgeInsets.only(
                                     top: 15.w, left: 15.w, right: 15.w),
-                                padding: EdgeInsets.all(15.w),
+                                padding: EdgeInsets.all(30.w),
                                 decoration: BoxDecoration(
                                   color: Colors.white,
                                   borderRadius:
@@ -107,9 +107,9 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                         decoration: BoxDecoration(
                                             border: Border(
                                                 bottom: BorderSide(
-                                                    width: 0.5.w,
+                                                    width: 1.w,
                                                     color: const Color.fromARGB(
-                                                        255, 207, 211, 212)))),
+                                                        255, 243, 243, 243)))),
                                         child: Row(
                                           mainAxisAlignment:
                                               MainAxisAlignment.spaceBetween,
@@ -120,12 +120,12 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                               children: [
                                                 Icon(
                                                   const IconData(
-                                                    0xed9e,
+                                                    0xe611,
                                                     fontFamily: 'Iconfont',
                                                   ), // 使用的图标
                                                   color: const Color.fromARGB(
                                                       255, 59, 143, 65), // 图标颜色
-                                                  size: 30.w, // 图标大小
+                                                  size: 32.w, // 图标大小
                                                 ),
                                                 SizedBox(
                                                   width: 10.w,
@@ -133,7 +133,7 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                                 Text(
                                                   "付款码",
                                                   style: TextStyle(
-                                                      fontSize: 30.w,
+                                                      fontSize: 32.w,
                                                       height: 1.08,
                                                       color:
                                                           const Color.fromARGB(
@@ -155,12 +155,12 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                                     right: 33.w), // 设置右侧内边距
                                                 child: Icon(
                                                   color: const Color.fromARGB(
-                                                      255, 64, 64, 64),
+                                                      255, 181, 181, 181),
                                                   const IconData(
                                                     0xe659,
                                                     fontFamily: 'Iconfont',
                                                   ),
-                                                  size: 37.w, // 图标大小
+                                                  size: 45.w, // 图标大小
                                                 ),
                                               ),
                                             )
@@ -169,14 +169,12 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                     SizedBox(
                                       height: 40.w,
                                     ),
-                                    Container(
-                                      child: Text(
-                                        "优先使用零钱付款",
-                                        style: TextStyle(
-                                            fontSize: 25.w,
-                                            color: const Color.fromARGB(
-                                                255, 157, 161, 162)),
-                                      ),
+                                    Text(
+                                      "优先使用零钱付款",
+                                      style: TextStyle(
+                                          fontSize: 25.w,
+                                          color: const Color.fromARGB(
+                                              255, 157, 161, 162)),
                                     ),
                                     SizedBox(
                                       height: 10.w,
@@ -192,18 +190,18 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                     ),
                                     Container(
                                         padding: EdgeInsets.only(bottom: 60.w),
-                                        height: 315.w,
+                                        height: 320.w,
                                         width: screenSize.width,
                                         decoration: BoxDecoration(
                                             border: Border(
                                                 bottom: BorderSide(
-                                                    width: 0.5.w,
+                                                    width: 1.w,
                                                     color: const Color.fromARGB(
                                                         255, 243, 243, 243)))),
                                         child: Image.asset(
                                           assetPath("images/avatar/qrcode.png"),
-                                          width: 315.0.w,
-                                          height: 315.0.w,
+                                          width: 320.0.w,
+                                          height: 320.0.w,
                                           fit: BoxFit.contain,
                                         )),
                                     SizedBox(
@@ -331,7 +329,32 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
                                 )),
 
                             SizedBox(
-                              height: 17.w,
+                              height: 10.w,
+                            ),
+
+                            Container(
+                                margin: EdgeInsets.only(
+                                    top: 15.w, left: 15.w, right: 15.w),
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15.w)),
+                                ),
+                                child: const Column(children: [
+                                  LJNCAPFunctionItem(
+                                    id: "5c620baa-7a31-5080-8e04-413f6c9d3c7a",
+                                    title: "数字人民币付款",
+                                    icon: 0xe6f5,
+                                    iconColor: Colors.red,
+                                    link: '',
+                                    color: Colors.black,
+                                    backgroundColor: Colors.white,
+                                    underline: false,
+                                  ),
+                                ])),
+
+                            SizedBox(
+                              height: 10.w,
                             ),
 
                             // 列表
@@ -404,6 +427,7 @@ class _LJNCollectionAndPaymentState extends State<LJNCollectionAndPayment> {
 class LJNCAPFunctionItem extends StatefulWidget {
   final String id;
   final int? icon;
+  final Color? iconColor;
   final double? height;
   final String title;
   final String? link;
@@ -417,6 +441,7 @@ class LJNCAPFunctionItem extends StatefulWidget {
     super.key,
     required this.id,
     this.icon,
+    this.iconColor,
     this.height,
     required this.title,
     this.link,
@@ -484,7 +509,7 @@ class _LJNCAPFunctionItemState extends State<LJNCAPFunctionItem> {
       child: Container(
         height: widget.height ?? 125.0.w,
         color: backgroundColor,
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        padding: EdgeInsets.symmetric(horizontal: 30.w),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.center,
@@ -499,7 +524,7 @@ class _LJNCAPFunctionItemState extends State<LJNCAPFunctionItem> {
                     widget.icon!,
                     fontFamily: 'Iconfont',
                   ), // 使用的图标
-                  color: Colors.white, // 图标颜色
+                  color: widget.iconColor ?? Colors.white, // 图标颜色
                   size: 35.w, // 图标大小
                 ),
               ),
@@ -514,10 +539,9 @@ class _LJNCAPFunctionItemState extends State<LJNCAPFunctionItem> {
                     border: Border(
                         bottom: BorderSide(
                   color: widget.underline
-                      ? lightenColor(
-                          const Color.fromARGB(255, 56, 179, 114), 0.2)
+                      ? const Color.fromARGB(255, 60, 182, 118)
                       : Colors.transparent,
-                  width: 1.w,
+                  width: 2.w,
                   style: BorderStyle.solid,
                 ))),
                 child: Row(
@@ -533,7 +557,7 @@ class _LJNCAPFunctionItemState extends State<LJNCAPFunctionItem> {
                             height: 1.08,
                             fontSize: fontSizeScale(32.0.w),
                             fontFamily: "AlibabaPuHuiTi",
-                            color: Colors.white),
+                            color: widget.color ?? Colors.white),
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -565,9 +589,8 @@ class _LJNCAPFunctionItemState extends State<LJNCAPFunctionItem> {
                                   : widget.showStyle as Widget)),
 
                     if (widget.link != null)
-                      Container(
+                      SizedBox(
                           width: 30.w,
-                          margin: const EdgeInsets.only(right: 32).w,
                           child: Icon(
                             const IconData(
                               0xed9d,
