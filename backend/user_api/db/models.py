11449f4f8d23e99226c1c6bea3ec4ada23f69541
Jum <565657400@qq.com>
2025-10-05 17:24:16 +0800

    增加分享

--- backend/user_api/db/models.py
+++ backend/user_api/db/models.py
@@ -5,6 +5,8 @@ from db.database import Base
 class VigaUsers(Base):
     __tablename__ = "viga_users"
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
     phone_number = Column(String(20), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now())
